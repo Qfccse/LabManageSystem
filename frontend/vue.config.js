64e4e3f101dcb4b77d856c0d02fd6fc169e1cfe5
ValarMorghulis <2544668476@qq.com>
2022-11-21 22:27:32 +0800

    build file in current folder

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -27,8 +27,8 @@ module.exports = {
     // open: true,//值为 true的话，项目启动时自动打开到浏览器里边， false不会打开
     proxy:{
       '/api':{
-        target:"http://localhost:8081/",//跨域请求的公共地址
-        // target:"http://localhost:10010/",//配好getway后使用这个
+        // target:"http://localhost:8081/",//跨域请求的公共地址
+        target:"http://localhost:10010/",//配好getway后使用这个
         ws:false, //也可以忽略不写，不写不会影响跨域
         changeOrigin:true, //是否开启跨域，值为 true 就是开启， false 不开启
         pathRewrite:{
